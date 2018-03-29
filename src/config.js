2fc99523ab6c08ce33727229e025282577ae1f13
Nazarov MI <nazarov-mi@mail.ru>
2018-03-30 02:00:34 +0300

    Изменит конфигурацию Package.json

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const { join } = require('path')
 
-const IS_PROD = false // process.env.NODE_ENV === 'production'
+const IS_PROD = process.env.NODE_ENV !== 'development'
 const DEFAULT_HASH = 'HEAD'
 const ROOT_PATH = join(__dirname, '../')
 const REPOSITORY_PATH = join(ROOT_PATH, IS_PROD ? 'local-repository' : 'local-test-repository')
-console.log(process.env.NODE_ENV)
+
 module.exports = {
 	IS_PROD,
 	DEFAULT_HASH,
